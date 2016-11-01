a5c6a2939c8617208fbbaaca1a764b827bba6fcd
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2016-10-31 20:53:39 -0400

    Create a window

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -4,6 +4,31 @@
 int main(int argc, char ** argv)
 {
   SDL_Init(SDL_INIT_VIDEO);
+  SDL_Window *window = SDL_CreateWindow(
+    "Tetris",
+    SDL_WINDOWPOS_UNDEFINED,
+    SDL_WINDOWPOS_UNDEFINED,
+    1024,
+    768,
+    SDL_WINDOW_OPENGL
+  );
+
+  bool running = true; 
+  while (running) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+        case SDL_KEYDOWN:
+          switch (event.key.keysym.sym) {
+            case SDLK_ESCAPE:
+              running = false;
+              break;
+          }
+      }
+    }
+  }
+
+  SDL_DestroyWindow(window);
   SDL_Quit();
   return 0;
 }
