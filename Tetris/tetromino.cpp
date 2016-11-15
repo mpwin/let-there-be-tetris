021e09f8cb20d6f3a4ff9c0c9339c72df43f365f
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2016-11-15 01:27:37 -0500

    Add shape setters to I tetromino subclass

--- Tetris/tetromino.cpp
+++ Tetris/tetromino.cpp
@@ -1,6 +1,64 @@
+#include <cstdlib>
+
 #include "tetromino.h"
 
-I::I() {}
+I::I() {
+  size = 4;
+  state = rand() % 4 + 1;
+
+  set_shape_1();
+  set_shape_2();
+  set_shape_3();
+  set_shape_4();
+}
+
+void I::set_shape_1() {
+  int shape[] = { 0, 1, 0, 0,
+                  0, 1, 0, 0,
+                  0, 1, 0, 0,
+                  0, 1, 0, 0 };
+
+  shape_1 = new int[size];
+  for (int i = 0; i < size; i++) {
+    shape_1[i] = shape[i];
+  }
+}
+
+void I::set_shape_2() {
+  int shape[] = { 0, 0, 0, 0,
+                  1, 1, 1, 1,
+                  0, 0, 0, 0,
+                  0, 0, 0, 0 };
+
+  shape_2 = new int[size];
+  for (int i = 0; i < size; i++) {
+    shape_2[i] = shape[i];
+  }
+}
+
+void I::set_shape_3() {
+  int shape[] = { 0, 0, 1, 0,
+                  0, 0, 1, 0,
+                  0, 0, 1, 0,
+                  0, 0, 1, 0 };
+
+  shape_3 = new int[size];
+  for (int i = 0; i < size; i++) {
+    shape_3[i] = shape[i];
+  }
+}
+
+void I::set_shape_4() {
+  int shape[] = { 0, 0, 0, 0,
+                  0, 0, 0, 0,
+                  1, 1, 1, 1,
+                  0, 0, 0, 0 };
+
+  shape_4 = new int[size];
+  for (int i = 0; i < size; i++) {
+    shape_4[i] = shape[i];
+  }
+}
 
 L::L() {}
 
--- Tetris/tetromino.h
+++ Tetris/tetromino.h
@@ -3,12 +3,19 @@
 
 class Tetromino {
 protected:
-  int shape;
+  int size;
+  int state;
+  int *shape_1, *shape_2, *shape_3, *shape_4;
 };
 
 class I : public Tetromino {
 public:
   I();
+private:
+  void set_shape_1();
+  void set_shape_2();
+  void set_shape_3();
+  void set_shape_4();
 };
 
 class L : public Tetromino {
