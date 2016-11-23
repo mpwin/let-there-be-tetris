cb07956879062f18f8904a91002a91fe1cc6f893
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2016-11-22 20:37:00 -0500

    Add border to tiles

--- Tetris/tile.cpp
+++ Tetris/tile.cpp
@@ -9,12 +9,22 @@ void Tile::draw()
 {
   if (state == 1)
   {
+    float border_width = 0.05f;
+
     glBegin(GL_QUADS);
-      glColor3f(0.02f, 0.60f, 0.02f);
+      glColor3f(0.00f, 0.00f, 0.00f);
       glVertex2f(x, y);
       glVertex2f(x, y + 1);
       glVertex2f(x + 1, y + 1);
       glVertex2f(x + 1, y);
+    glEnd();
+
+    glBegin(GL_QUADS);
+      glColor3f(0.02f, 0.60f, 0.02f);
+      glVertex2f(x + border_width, y + border_width);
+      glVertex2f(x + border_width, y + 1 - border_width);
+      glVertex2f(x + 1 - border_width, y + 1 - border_width);
+      glVertex2f(x + 1 - border_width, y + border_width);
     glEnd();
   }
 }
